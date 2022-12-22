f94b77f6efcd78fe890f39616d3740f2f21b0614
seoninja13 <dachevivo@gmail.com>
2022-12-22 15:22:26 -0800

    first code is in

--- Net6-scrape-test/Program.cs
+++ Net6-scrape-test/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Scrape books!");
+    
+
+static HtmlDocument GetDocument(string url)
+{
+    Html web = new HtmlWeb();
+    HtmlDocument doc = web.Load(url);
+    console.WriteLine("Url is: " + url);
+    return doc;
+}
+
+public HtmlNodeCollection SelectNodes(string xpath);
+
+public HtmlNode SelectSingleNode(string xpath);
